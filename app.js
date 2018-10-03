6dd116f2c9a415b667f2110c5a7f8d50f9d13ec7
Cynthia <chicagock@gmail.com>
2018-10-03 17:22:42 -0400

    october 3 revisions

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 
-const render = function() {
-    $('.content').on();
-    for( let i = 0; i < employeeList.length; i++ ) {
-      $('.content').on(`${employeeList[i]}`);
-    }
- 
-document.getElementById("view").addEventListener("click", render);
+$(document).ready(function() {
+
+$('.btn').on('click', text);}
+
+// document.querySelector('.content').textContent = 'hey';
+
+// const exampleList = document.querySelectorAll('.view');
+// console.dir(exampleList);
+
+// for (let i = 0; i < exampleList.length; i++){
+//     exampleList[i].innerText = 'All'
+
+// // const render = function() {
+//     $('.content').on();
+//     for( let i = 0; i < employeeList.length; i++ ) {
+// //       $('.content').on(`${employeeList[i]}`);
+//     }
+// const getVal = function(event){
+//   event.preventDefault();
+//   console.log($('#name').val());
+// }
+
+// $('#search').on('click', getVal);
+
+$("#view").on("click", );
 }
 
 
 // function myFunction() {
 //     document.getElementById("view").console.log(employeeList);
-    
-
-
+  

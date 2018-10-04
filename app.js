5c0678ba0ad52b9a23af50a7db6ef069728bb6c8
Cynthia <chicagock@gmail.com>
2018-10-03 22:03:11 -0400

    adding employee list view with bootstrap table view

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
-
-$(document).ready(function() {
-
-$('.btn').on('click', text);}
-
-// document.querySelector('.content').textContent = 'hey';
-
-// const exampleList = document.querySelectorAll('.view');
-// console.dir(exampleList);
-
-// for (let i = 0; i < exampleList.length; i++){
-//     exampleList[i].innerText = 'All'
-
-// // const render = function() {
-//     $('.content').on();
-//     for( let i = 0; i < employeeList.length; i++ ) {
-// //       $('.content').on(`${employeeList[i]}`);
-//     }
-// const getVal = function(event){
-//   event.preventDefault();
-//   console.log($('#name').val());
-// }
-
-// $('#search').on('click', getVal);
-
-$("#view").on("click", );
-}
-
-
-// function myFunction() {
-//     document.getElementById("view").console.log(employeeList);
-  
+// view function click to revel employee list table- showing each info title
+const viewFunction = function () {
+    console.log("view button was clicked");
+    for (let i = 0; i < employeeList.length; i++) {
+    $("#content").append(`<div class="container">
+    <div class="table">
+    <table class="table table-bordered">
+    <thead> <tr>
+    <th scope="col">Name: </th>
+    <th scope="col">Office Number: </th>
+    <th scope="col">Phone Number: </th>
+    </tr> </thead>
+    <tbody>
+    <tr>
+    <td>
+    ${employeeList[i].name}
+    </td>
+    <td>
+    ${employeeList[i].officeNum}
+    </td>
+    <td>
+    ${employeeList[i].phoneNum}
+    </td>
+    </tr>
+    </div>
+    </div>`);
+    }
+    }
+    
+    $("#view").on("click", viewFunction)
+    
+    // add function click
+    const addFunction = function () {
+    console.log("button was clicked");
+    
+    }
+    
+    $("#add").on("click", addFunction)
+    
+    
+    // update function click
+    const updateFunction = function () {
+    console.log("button was clicked");
+    }
+    
+    $("#update").on("click", updateFunction)
+    
+    // delete function click
+    const deleteFunction = function () {
+    console.log("button was clicked");
+    }
+    
+    $("#delete").on("click", deleteFunction)
+    
+    
+    // verify function click /////
+    const verifyFunction = function () {
+    console.log("button was clicked");
+    }
+    
+    $("#verify").on("click", verifyFunction)
